b6b84b87b528136b9a829f77c10ce3755963870d
oglesby80 <OglesbyRodriguez@gmail.com>
2024-08-28 20:14:13 -0400

    2nd push

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 });
