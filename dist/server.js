c3641c961486055fb841b1bbe857ab136eb7442f
oglesby80 <OglesbyRodriguez@gmail.com>
2024-08-30 15:50:22 -0400

    17 push

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url'; // Import for working with module URLs
+import { fileURLToPath } from 'url';
 const app = express();
 // Get the directory name for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Serve static files from the client/dist directory
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
-const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+const PORT = process.env.PORT || 4003;
 // Handle all routes by serving the index.html file
 app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
 });
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
